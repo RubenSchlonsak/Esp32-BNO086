03fe7f80cfe65ce5e95cf214fbf76a7eb965af3b
Ruben Schlonsak <ruben.schlonsak@th-luebeck.de>
2025-11-03 09:04:58 +0100

    changed to raw

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,61 +2,66 @@
 #include <Wire.h>
 #include <Adafruit_BNO08x.h>
 
-#define SDA_PIN    9
-#define SCL_PIN    8
-#define BNO_ADDR   0x4B
-#define REPORT_US  5000      // 200 Hz
+#define SDA_PIN 9
+#define SCL_PIN 8
 
 Adafruit_BNO08x bno(-1);
-static bool hostConnected = false;
-
-void printHeader() {
-  Serial.println("seq,t_ms,ax,ay,az");
-}
 
 void setup() {
   Serial.begin(921600);
-  Serial.setTxTimeoutMs(20);        // verhindert Dauer-Blockieren
-  delay(300);
-
+  while(!Serial) delay(10);
+  
+  Serial.println("\n\n# BNO086 Test Start");
+  
   Wire.begin(SDA_PIN, SCL_PIN);
-  Wire.setClock(400000);
-
-  if (!bno.begin_I2C(BNO_ADDR, &Wire)) {
+  
+  if (!bno.begin_I2C(0x4B, &Wire)) {
     if (!bno.begin_I2C(0x4A, &Wire)) {
-      // Kein endloses Printen, wenn kein Host dran
-      for (;;) delay(1000);
+      Serial.println("# FAIL: Sensor not found!");
+      while(1) delay(100);
     }
   }
-  bno.enableReport(SH2_ACCELEROMETER, REPORT_US);
+  
+  Serial.println("# OK: Sensor found");
+  
+  // Versuche verschiedene Sensoren
+  Serial.println("# Trying ACCELEROMETER...");
+  bno.enableReport(SH2_ACCELEROMETER, 10000); // 100Hz
+  
+  Serial.println("# Trying GYROSCOPE...");
+  bno.enableReport(SH2_GYROSCOPE_CALIBRATED, 10000);
+  
+  Serial.println("# Setup done\n");
 }
 
 void loop() {
-  // Verbindungstatus erkennen (CDC setzt das korrekt)
-  bool now = (bool)Serial;
-  if (now && !hostConnected) {
-    // frisch verbunden -> Header einmal senden
-    printHeader();
-  }
-  hostConnected = now;
-
   sh2_SensorValue_t v;
-  while (bno.getSensorEvent(&v)) {
-    if (v.sensorId != SH2_ACCELEROMETER) continue;
-
-    // FIFO immer leeren, aber nur senden wenn Host verbunden
-    if (hostConnected) {
-      // nur senden, wenn genug Buffer frei (verhindert Blockieren)
-      if (Serial.availableForWrite() >= 96) {
-        static uint32_t seq=0;
-        uint32_t t = millis();
-        Serial.printf("%lu,%lu,%.7f,%.7f,%.7f\n",
-                      (unsigned long)seq++,
-                      (unsigned long)t,
-                      v.un.accelerometer.x,
-                      v.un.accelerometer.y,
-                      v.un.accelerometer.z);
-      }
+  
+  if (bno.getSensorEvent(&v)) {
+    Serial.print("SensorID: ");
+    Serial.print(v.sensorId);
+    Serial.print(" | ");
+    
+    switch(v.sensorId) {
+      case SH2_ACCELEROMETER:
+        Serial.printf("Accel: %.3f, %.3f, %.3f\n",
+                     v.un.accelerometer.x,
+                     v.un.accelerometer.y,
+                     v.un.accelerometer.z);
+        break;
+        
+      case SH2_GYROSCOPE_CALIBRATED:
+        Serial.printf("Gyro: %.3f, %.3f, %.3f\n",
+                     v.un.gyroscope.x,
+                     v.un.gyroscope.y,
+                     v.un.gyroscope.z);
+        break;
+        
+      default:
+        Serial.println("Unknown sensor");
+        break;
     }
   }
+  
+  delay(1); // Kleine Pause
 }
